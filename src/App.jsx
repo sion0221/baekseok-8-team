64711b471e68984f69ab3ade1040f08eb93cab45
sionheo <tldhs123e@gmail.com>
2026-04-13 16:23:37 +0900

    init : 프로젝트 초기 파일 구성

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl">임시 페이지</h1>
+      <p className="text-bold">페이지 제작 중...</p>
+    </>
+  );
+}
+
+export default App;
